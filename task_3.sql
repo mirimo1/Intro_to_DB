f750faa05f950a60ca92bd2f49324a69c2dffbd9
Pascal Mirimo <pascalmirimo57@gmail.com>
2025-08-16 06:18:20 -0700

    Add SQL to list all tables in alx_book_store

--- task_3.sql
+++ task_3.sql
@@ -1,2 +1,8 @@
+-- Select the database
+USE alx_book_store;
+
 -- List all tables in the database
-SHOW TABLES;
+SELECT TABLE_NAME 
+FROM INFORMATION_SCHEMA.TABLES 
+WHERE TABLE_TYPE = 'BASE TABLE' 
+  AND TABLE_SCHEMA = 'alx_book_store';
