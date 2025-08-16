c9b4dfaa46b071b9bec619477ecdc23434a9e107
Pascal Mirimo <pascalmirimo57@gmail.com>
2025-08-16 06:26:41 -0700

    Add SQL to display full description of books table

---
+++ task_4.sql
@@ -0,0 +1,16 @@
+-- task_4.sql
+-- Show full description of the 'books' table
+
+USE alx_book_store;
+
+SELECT 
+    COLUMN_NAME AS 'Column Name',
+    COLUMN_TYPE AS 'Data Type',
+    IS_NULLABLE AS 'Nullable',
+    COLUMN_KEY AS 'Key',
+    COLUMN_DEFAULT AS 'Default',
+    EXTRA AS 'Extra'
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store'
+  AND TABLE_NAME = 'books'
+ORDER BY ORDINAL_POSITION;
