f8ccbe7f232e7a8c471f05d58f1bc950cbd0fc2b
Pascal Mirimo <pascalmirimo57@gmail.com>
2025-08-16 04:25:06 -0700

    Add alx_book_store database with tables

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -16,6 +16,14 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Current Database: `alx_book_store`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `alx_book_store` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+USE `alx_book_store`;
+
+--
 -- Table structure for table `authors`
 --
 
@@ -155,4 +163,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-08-16  4:19:49
+-- Dump completed on 2025-08-16  4:24:45
