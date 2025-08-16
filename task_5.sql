d1b785c860297adf5434d04607e0259f33c6fc7c
Pascal Mirimo <pascalmirimo57@gmail.com>
2025-08-16 06:32:38 -0700

    Fix table name to singular 'customer' for insertion

--- task_5.sql
+++ task_5.sql
@@ -1,7 +1,7 @@
 -- task_5.sql
--- Insert a single row into the customers table
+-- Insert a single row into the customer table
 
 USE alx_book_store;
 
-INSERT INTO CUSTOMERS (customer_id, customer_name, email, address)
+INSERT INTO customer (customer_id, customer_name, email, address)
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
